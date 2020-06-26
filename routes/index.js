83da56fdec835ab4bd89ddd59d488f3d1ccf19b5
Adrian Vinas <adrianvinasrosa@gmail.com>
2020-06-26 11:56:53 -0400

    conexion a la base de datos agregada

---
+++ routes/index.js
@@ -0,0 +1,7 @@
+const express = require('express');
+const router = express.Router();
+
+//probando la conexion a la base de datos
+const db = require('../db_connection.js');
+
+module.exports = router;
--- server.js
+++ server.js
@@ -1,13 +1,16 @@
 const express = require('express');
-//HTTP request logger middleware for node.js
+//Middleware para ver las peticiones al servidor en la consola
 const morgan = require('morgan');
 
+//inicializacion del servidor
 const app = express();
 
 
 //middleware
 app.use(morgan('dev'));
 
+//routes
+app.use(require('./routes/index.js'));
 
 //Inicio del servidor
 app.listen(3000, ()=>{
